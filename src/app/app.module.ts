8b3e558b10596ce1b6aa7d0d15302ba2ebb1a222
gauri23092002 <gauri23092002@gmail.com>
2022-07-06 10:54:06 +0530

    restaurant searching

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginpageComponent } from './loginpage/loginpage.component';
+import { SearchRestaurantComponent } from './search-restaurant/search-restaurant.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginpageComponent
+    LoginpageComponent,
+    SearchRestaurantComponent
   ],
   imports: [
     BrowserModule,
